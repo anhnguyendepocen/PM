2ca01f867790b20bf2513b7052d594162dbb27c4
Myles Gartland <myles.gartland@rockhurst.edu>
2016-08-21 17:18:47 -0500

    initial

---
+++ week 1/lasso.r
@@ -0,0 +1,11 @@
+#lasso
+library(glmnet)
+# Gaussian
+x=matrix(rnorm(100*20),100,20)
+y=rnorm(100)
+fit1=glmnet(x,y)
+print(fit1)
+coef(fit1,s=0.01) # extract coefficients at a single value of lambda
+predict(fit1,newx=x[1:10,],s=c(0.01,0.005)) # make predictions
+
+
